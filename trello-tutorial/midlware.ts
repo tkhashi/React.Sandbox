f19bbe633a36d605a0cd00815c5886aa18e22d3d
Kazuhiro Takahashi <pupodenews@gmail.com>
2024-03-28 16:55:43 +0900

    End select-organization 2:04:45

--- trello-tutorial/midlware.ts
+++ trello-tutorial/midlware.ts
@@ -1,24 +1,38 @@
-import { authMiddleware } from "@clerk/nextjs";
- 
-// See https://clerk.com/docs/references/nextjs/auth-middleware
-// for more information about configuring your Middleware
- 
+import { authMiddleware, redirectToSignIn } from "@clerk/nextjs";
+import { NextResponse } from "next/server";
+
+// This example protects all routes including api/trpc routes
+// Please edit this to allow other routes to be public as needed.
+// See https://clerk.com/docs/references/nextjs/auth-middleware for more information about configuring your Middleware
 export default authMiddleware({
-  // Allow signed out users to access the specified routes:
-  // publicRoutes: ['/anyone-can-visit-this-route'],
-  // Prevent the specified routes from accessing
-  // authentication information:
-  // ignoredRoutes: ['/no-auth-in-this-route'],
-  publicRoutes: ["/"]
+	publicRoutes: ["/"],
+	afterAuth(auth, req) {
+		if (auth.userId && auth.isPublicRoute) {
+			let path = "/select-org";
+
+			if (auth.orgId) {
+				path = `/organization/${auth.orgId}`;
+			}
+
+			const orgSelection = new URL(path, req.url);
+			return NextResponse.redirect(orgSelection);
+		}
+
+		if (!auth.userId && !auth.isPublicRoute) {
+			return redirectToSignIn({ returnBackUrl: req.url });
+		}
+
+		if (
+			auth.userId &&
+			!auth.orgId &&
+			req.nextUrl.pathname !== "/select-org"
+		) {
+			const orgSelection = new URL("/select-org", req.url);
+			return NextResponse.redirect(orgSelection);
+		}
+	},
 });
- 
+
 export const config = {
-  matcher: [
-    // Exclude files with a "." followed by an extension, which are typically static files.
-    // Exclude files in the _next directory, which are Next.js internals.
- 
-    "/((?!.+\\.[\\w]+$|_next).*)",
-    // Re-include any files in the api or trpc folders that might have an extension
-    "/(api|trpc)(.*)"
-  ]
+	matcher: ["/((?!.+\\.[\\w]+$|_next).*)", "/", "/(api|trpc)(.*)"],
 };
